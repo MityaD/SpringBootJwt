9ca2d9fb7523ce4ae6e5dbdb073fb2875fe080f0
Dima Yakushuk <yakushyk.dmitrii@gmail.com>
2021-10-24 21:44:02 +0300

    first commit

---
+++ src/test/java/com/yakut/springbootjwt/SpringBootJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yakut.springbootjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
